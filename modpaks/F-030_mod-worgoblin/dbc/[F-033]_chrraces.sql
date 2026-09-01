3a649fa83418d01c7bb74f350eb81968c9524b77
Medviten <medviten@protonmail.com>
2026-09-01 14:18:50 +0200

    Cleaned up some more

--- modpaks/F-030_mod-worgoblin/dbc/[F-033]_chrraces.sql
+++
@@ -1,8 +0,0 @@
--- chrraces: 0 inserts, 1 updates, 0 deletes
-
-SET @Goblin = 9;
-SET @Worgen = 12;
-
--- New entries
-UPDATE `chrraces` SET `faction_id` = 2238 WHERE `id` = @Goblin;
-UPDATE `chrraces` SET `faction_id` = 2239 WHERE `id` = @Worgen;
