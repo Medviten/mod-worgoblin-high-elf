118cef3f04e216efc123023eccc7b98104ab65c4
Medviten <medviten@protonmail.com>
2026-09-01 18:08:02 +0200

    Disabled deletes to establish dupes.

--- modpaks/F-030_mod-worgoblin/dbc/[BASE,F-030]_charhairtextures.sql
+++ modpaks/F-030_mod-worgoblin/dbc/[BASE,F-030]_charhairtextures.sql
@@ -1,15 +1,15 @@
 -- charhairtextures: 6 inserts, 0 updates, 0 deletes
 
 -- New entries
-DELETE FROM `charhairtextures` WHERE `id` = 102;
+-- DELETE FROM `charhairtextures` WHERE `id` = 102;
 INSERT INTO `charhairtextures` (`id`, `race`, `gender`, `unk_1`, `unk_2`, `unk_3`, `unk_4`, `unk_5`) VALUES (102, 9, 0, 0, 255, 0, 1, 1);
-DELETE FROM `charhairtextures` WHERE `id` = 103;
+-- DELETE FROM `charhairtextures` WHERE `id` = 103;
 INSERT INTO `charhairtextures` (`id`, `race`, `gender`, `unk_1`, `unk_2`, `unk_3`, `unk_4`, `unk_5`) VALUES (103, 9, 1, 0, 255, 0, 1, 1);
-DELETE FROM `charhairtextures` WHERE `id` = 104;
+-- DELETE FROM `charhairtextures` WHERE `id` = 104;
 INSERT INTO `charhairtextures` (`id`, `race`, `gender`, `unk_1`, `unk_2`, `unk_3`, `unk_4`, `unk_5`) VALUES (104, 20, 0, 0, 255, 0, 1, 1);
-DELETE FROM `charhairtextures` WHERE `id` = 106;
+-- DELETE FROM `charhairtextures` WHERE `id` = 106;
 INSERT INTO `charhairtextures` (`id`, `race`, `gender`, `unk_1`, `unk_2`, `unk_3`, `unk_4`, `unk_5`) VALUES (106, 20, 1, 0, 255, 0, 1, 1);
-DELETE FROM `charhairtextures` WHERE `id` = 107;
+-- DELETE FROM `charhairtextures` WHERE `id` = 107;
 INSERT INTO `charhairtextures` (`id`, `race`, `gender`, `unk_1`, `unk_2`, `unk_3`, `unk_4`, `unk_5`) VALUES (107, 21, 0, 0, 255, 0, 1, 1);
-DELETE FROM `charhairtextures` WHERE `id` = 108;
+-- DELETE FROM `charhairtextures` WHERE `id` = 108;
 INSERT INTO `charhairtextures` (`id`, `race`, `gender`, `unk_1`, `unk_2`, `unk_3`, `unk_4`, `unk_5`) VALUES (108, 21, 1, 0, 255, 0, 1, 1);
