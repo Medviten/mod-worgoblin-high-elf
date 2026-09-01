bc02a75cc34debf4dd2826e7ac9bc922c5049806
Medviten <medviten@protonmail.com>
2026-09-01 22:55:05 +0200

    Moved some module-specific files

--- data/sql/db-world/base/quest_template.sql
+++
@@ -1,5 +0,0 @@
-/* Allows any race to complete any class-specific quest (ARAC, sweeping approach) */
-UPDATE `quest_template` INNER JOIN `quest_template_addon`
-	ON `quest_template_addon`.id = `quest_template`.id
-		SET `AllowableRaces` = `AllowableRaces` | 1 | 2 | 4 | 8 | 16 | 32 | 64 | 128 | 512 | 1024
-			WHERE `quest_template_addon`.allowableclasses != 0 AND AllowableRaces != 0;
