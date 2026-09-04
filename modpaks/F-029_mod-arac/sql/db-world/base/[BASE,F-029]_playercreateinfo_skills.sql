72e3f8b675c13f11bb0531d7d8d8f9efe5706e6d
Medviten <medviten@protonmail.com>
2026-09-05 01:41:54 +0200

    Fixed brackets

--- modpaks/F-029_mod-arac/sql/db-world/base/[BASE,F-029]_playercreateinfo_skills.sql
+++ modpaks/F-029_mod-arac/sql/db-world/base/[BASE,F-029]_playercreateinfo_skills.sql
@@ -5,7 +5,7 @@ UPDATE `playercreateinfo_skills` SET `raceMask` = @HumanMask | @DwarfMask | @Tau
 UPDATE `playercreateinfo_skills` SET `raceMask` = @PlayableRaceMask & ~(@TaurenMask | @DraeneiMask) WHERE `skill` = 173 AND `classMask` != 0;
 
 DELETE FROM `playercreateinfo_skills` WHERE `raceMask` = @TaurenMask | @DraeneiMask AND `classMask` = @RogueMask AND `skill` = 173;
-INSERT INTO `playercreateinfo_skills` VALUES (@TaurenMask | @DraeneiMask), @RogueMask, 173, 0, 'Daggers (Tauren & Draenei Rogues)';
+INSERT INTO `playercreateinfo_skills` VALUES (@TaurenMask | @DraeneiMask, @RogueMask, 173, 0, 'Daggers (Tauren & Draenei Rogues)');
 
 -- Classic ARAC approach equalizes skills across races instead
 /* UPDATE `playercreateinfo_skills` SET `raceMask` = 0 WHERE `skill` IN (45, 46, 160, 173, 226) AND `classMask` != 0; */
