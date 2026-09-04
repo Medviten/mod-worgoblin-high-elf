93f61cf46ffc33e4c13c69e91457977ccbf604e8
Medviten <medviten@protonmail.com>
2026-09-04 09:21:01 +0200

    Fixed wrong table in chrraces

--- modpaks/F-002_move-npc-races/dbc/[BASE,F-002]_chrraces.sql
+++ modpaks/F-002_move-npc-races/dbc/[BASE,F-002]_chrraces.sql
@@ -1,12 +1,12 @@
 -- [F-030] move_npc_races: chrraces: 0 inserts, 10 updates, 0 deletes
 
-UPDATE `charsections` SET `id` = @FelOrc            WHERE `id` = 12;
-UPDATE `charsections` SET `id` = @Naga              WHERE `id` = 13;
-UPDATE `charsections` SET `id` = @Skeleton          WHERE `id` = 14;
-UPDATE `charsections` SET `id` = @Vrykul            WHERE `id` = 15;
-UPDATE `charsections` SET `id` = @Tuskarr           WHERE `id` = 16;
-UPDATE `charsections` SET `id` = @ForestTroll       WHERE `id` = 17;
-UPDATE `charsections` SET `id` = @Tuskarr           WHERE `id` = 18;
-UPDATE `charsections` SET `id` = @Taunka            WHERE `id` = 19;
-UPDATE `charsections` SET `id` = @NorthrendSkeleton WHERE `id` = 20;
-UPDATE `charsections` SET `id` = @IceTroll          WHERE `id` = 21;
+UPDATE `chrraces` SET `id` = @FelOrc            WHERE `id` = 12;
+UPDATE `chrraces` SET `id` = @Naga              WHERE `id` = 13;
+UPDATE `chrraces` SET `id` = @Skeleton          WHERE `id` = 14;
+UPDATE `chrraces` SET `id` = @Vrykul            WHERE `id` = 15;
+UPDATE `chrraces` SET `id` = @Tuskarr           WHERE `id` = 16;
+UPDATE `chrraces` SET `id` = @ForestTroll       WHERE `id` = 17;
+UPDATE `chrraces` SET `id` = @Tuskarr           WHERE `id` = 18;
+UPDATE `chrraces` SET `id` = @Taunka            WHERE `id` = 19;
+UPDATE `chrraces` SET `id` = @NorthrendSkeleton WHERE `id` = 20;
+UPDATE `chrraces` SET `id` = @IceTroll          WHERE `id` = 21;
