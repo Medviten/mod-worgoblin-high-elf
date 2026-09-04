d51bf15620d000920f2d94336c9ca58ce22c6f9c
Medviten <medviten@protonmail.com>
2026-09-04 08:55:52 +0200

    Disabled more deletes.

--- modpaks/F-002_move-npc-races/dbc/[BASE,F-002]_charsections.sql
+++ modpaks/F-002_move-npc-races/dbc/[BASE,F-002]_charsections.sql
@@ -1,15 +1,15 @@
 -- [F-030] mod-worgoblin: charsections
 
-UPDATE `charsections` SET `id` = `id` + 32264, `race` = @FelOrc            WHERE `race` = 12;
-UPDATE `charsections` SET `id` = `id` + 32264, `race` = @Naga              WHERE `race` = 13;
-UPDATE `charsections` SET `id` = `id` + 32264, `race` = @Skeleton          WHERE `race` = 14;
-UPDATE `charsections` SET `id` = `id` + 32264, `race` = @Vrykul            WHERE `race` = 15;
-UPDATE `charsections` SET `id` = `id` + 32264, `race` = @Tuskarr           WHERE `race` = 16;
-UPDATE `charsections` SET `id` = `id` + 32264, `race` = @ForestTroll       WHERE `race` = 17;
-UPDATE `charsections` SET `id` = `id` + 32264, `race` = @Tuskarr           WHERE `race` = 18;
-UPDATE `charsections` SET `id` = `id` + 32264, `race` = @Taunka            WHERE `race` = 19;
-UPDATE `charsections` SET `id` = `id` + 32264, `race` = @NorthrendSkeleton WHERE `race` = 20;
-UPDATE `charsections` SET `id` = `id` + 32264, `race` = @IceTroll          WHERE `race` = 21;
+UPDATE `charsections` SET `race` = @FelOrc            WHERE `race` = 12;
+UPDATE `charsections` SET `race` = @Naga              WHERE `race` = 13;
+UPDATE `charsections` SET `race` = @Skeleton          WHERE `race` = 14;
+UPDATE `charsections` SET `race` = @Vrykul            WHERE `race` = 15;
+UPDATE `charsections` SET `race` = @Tuskarr           WHERE `race` = 16;
+UPDATE `charsections` SET `race` = @ForestTroll       WHERE `race` = 17;
+UPDATE `charsections` SET `race` = @Tuskarr           WHERE `race` = 18;
+UPDATE `charsections` SET `race` = @Taunka            WHERE `race` = 19;
+UPDATE `charsections` SET `race` = @NorthrendSkeleton WHERE `race` = 20;
+UPDATE `charsections` SET `race` = @IceTroll          WHERE `race` = 21;
 
 /*
 Original range:
