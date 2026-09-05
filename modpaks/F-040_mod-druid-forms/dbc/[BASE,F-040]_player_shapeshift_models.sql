4a21d1b98169ae8ca16a4fd403bde370c2781745
Medviten <medviten@protonmail.com>
2026-09-06 01:04:07 +0200

    Fixed SQL syntax.

--- modpaks/F-040_mod-druid-forms/dbc/[BASE,F-040]_player_shapeshift_models.sql
+++ modpaks/F-040_mod-druid-forms/dbc/[BASE,F-040]_player_shapeshift_models.sql
@@ -1,5 +1,11 @@
 -- Moved from Worgoblin (not really relevant to that module)
-
+REPLACE INTO `player_shapeshift_model` (
+    `ShapeshiftID`, -- ID from SpellShapeshiftForm.dbc or spellshapeshiftform_dbc
+    `RaceID`, -- ID from ChrRaces.dbc or chrraces_dbc
+    `CustomizationID`, -- hair colour or skin colour
+    `GenderID`, -- 0: male, 1: female, 2: both
+    `ModelID` -- ID from CreatureDisplayInfo.dbc or creaturedisplayinfo_dbc (*not* from CreatureModelData.dbc!)
+) VALUES
 /* Troll Druid forms */
 (@CatForm,         @Troll,   0, 2, 33665), -- pink hair: DruidCatTrollSkinBlack
 (@CatForm,         @Troll,   1, 2, 33668), -- red hair: DruidCatTrollSkinRed
