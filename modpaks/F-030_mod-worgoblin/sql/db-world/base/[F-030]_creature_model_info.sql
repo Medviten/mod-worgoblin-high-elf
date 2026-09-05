39e08912f21e8fbd4ac5643deeee673cc8b10497
Medviten <medviten@protonmail.com>
2026-09-06 00:41:41 +0200

    Added creaturemodeldata back into the pipeline

--- modpaks/F-030_mod-worgoblin/sql/db-world/base/[F-030]_creature_model_info.sql
+++ modpaks/F-030_mod-worgoblin/sql/db-world/base/[F-030]_creature_model_info.sql
@@ -1,7 +1,7 @@
 /* Add model info for mounts and other NPCs */
-DELETE FROM `creature_model_info` WHERE `DisplayID` IN (94133, 94134, 94135, 94136);
+DELETE FROM `creature_model_info` WHERE `DisplayID` IN (@WorgenWildMaleDisplay, @WorgenWildFemaleDisplay, @GilneanMaleDisplay, @GilneanFemaleDisplay);
 INSERT INTO `creature_model_info` (`DisplayID`, `BoundingRadius`, `CombatReach`, `Gender`, `DisplayID_Other_Gender`) VALUES
-(94133, 0.406, 1.5, 0, 0), -- Running Wild Male
-(94134, 0.406, 1.5, 1, 0), -- Running Wild Female
-(94135, 0.306, 1.5, 0, 0), -- Gilnean Male
-(94136, 0.306, 1.5, 1, 0); -- Gilnean Female
+(@WorgenWildMaleDisplay,   0.406, 1.5, 0, 0), -- Running Wild Male
+(@WorgenWildFemaleDisplay, 0.406, 1.5, 1, 0), -- Running Wild Female
+(@GilneanMaleDisplay,      0.306, 1.5, 0, 0), -- Gilnean Male
+(@GilneanFemaleDisplay,    0.306, 1.5, 1, 0); -- Gilnean Female
