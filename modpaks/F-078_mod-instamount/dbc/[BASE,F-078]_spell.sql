ace95d25f47960e5cf12b615518ebab9570d58c2
Medviten <medviten@protonmail.com>
2026-09-07 03:23:24 +0200

    Experimental indoors mounting

--- modpaks/F-078_mod-instamount/dbc/[BASE,F-078]_spell.sql
+++ modpaks/F-078_mod-instamount/dbc/[BASE,F-078]_spell.sql
@@ -2,3 +2,4 @@
 
 -- Makes mounts instacasts
 UPDATE `spell` SET `cast_time_index` = 1 WHERE `effect_1` = 6 AND `effect_apply_aura_name_1` = 78;
+UPDATE `spell` SET `attributes` = `attributes` & ~32768 WHERE `effect_1` = 6 AND `effect_apply_aura_name_1` = 78;
