4f7745d90a0f1c05e4ec7ea6c339c72183b7b2b5
Medviten <medviten@protonmail.com>
2026-09-07 03:03:48 +0200

    Fixed filenames and column names

--- modpaks/F-077_mod-instamount/dbc/[BASE,F-076]_spell.sql
+++
@@ -1,4 +0,0 @@
--- [F-076]_mod-instamount: spell
-
--- Makes mounts instacasts
-UPDATE `spell` SET `cast_time_index` = 1 WHERE `effect_1` = 6 AND `effectaura_1` = 78;
---
+++ modpaks/F-078_mod-instamount/dbc/[BASE,F-078]_spell.sql
@@ -0,0 +1,4 @@
+-- [F-078]_mod-instamount: spell
+
+-- Makes mounts instacasts
+UPDATE `spell` SET `cast_time_index` = 1 WHERE `effect_1` = 6 AND `effect_apply_aura_name_1` = 78;
