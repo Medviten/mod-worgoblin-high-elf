2b0e36142be368bb8369ca04a1baa55d680a7d6b
Medviten <medviten@protonmail.com>
2026-09-07 03:47:09 +0200

    Added Running Wild to the instamount/indoors list

--- modpaks/F-078_mod-instamount/dbc/[BASE,F-078]_spell.sql
+++ modpaks/F-078_mod-instamount/dbc/[BASE,F-078]_spell.sql
@@ -2,9 +2,11 @@
 
 -- Makes mounts instacasts
 UPDATE `spell` SET `cast_time_index` = 1 WHERE `effect_1` = 6 AND `effect_apply_aura_name_1` = 78;
+UPDATE `spell` SET `cast_time_index` = 1 WHERE `id` IN (87840, 87841, 110010, 110011); -- Running Wild
 
 -- Indoors mountup
 UPDATE `spell` SET `attributes` = `attributes` & ~32768 WHERE `effect_1` = 6 AND `effect_apply_aura_name_1` = 78;
+UPDATE `spell` SET `attributes` = `attributes` & ~32768 WHERE `id` IN (87840, 87841, 110010, 110011); -- Running Wild
 
 -- Indoors Druid Travel, Aquatic and Flight Forms (for fairness)
 UPDATE `spell` SET `attributes` = `attributes` & ~32768 WHERE `id` IN (783, 1066, 33943, 40120);
