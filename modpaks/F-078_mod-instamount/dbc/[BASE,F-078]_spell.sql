92833d03298d9927cc248943939ef3d6db20509f
Medviten <medviten@protonmail.com>
2026-09-07 03:41:25 +0200

    Added Druid forms to the indoors mount spells

--- modpaks/F-078_mod-instamount/dbc/[BASE,F-078]_spell.sql
+++ modpaks/F-078_mod-instamount/dbc/[BASE,F-078]_spell.sql
@@ -2,4 +2,9 @@
 
 -- Makes mounts instacasts
 UPDATE `spell` SET `cast_time_index` = 1 WHERE `effect_1` = 6 AND `effect_apply_aura_name_1` = 78;
+
+-- Indoors mountup
 UPDATE `spell` SET `attributes` = `attributes` & ~32768 WHERE `effect_1` = 6 AND `effect_apply_aura_name_1` = 78;
+
+-- Indoors Druid Travel, Aquatic and Flight Forms (for fairness)
+UPDATE `spell` SET `attributes` = `attributes` & ~32768 WHERE `id` IN (783, 1066, 33943, 40120);
