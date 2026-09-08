ed5eef1a85d4845757406b6a3f1cdffa408fca03
Medviten <medviten@protonmail.com>
2026-09-08 19:34:03 +0200

    Changed dwarf to troll

--- modpaks/F-035_mod-zandalari-troll/sql/db-world/base/[BASE,F-035]_playercreateinfo_skills.sql
+++ modpaks/F-035_mod-zandalari-troll/sql/db-world/base/[BASE,F-035]_playercreateinfo_skills.sql
@@ -1,7 +1,7 @@
 -- All normal dwarf starting skills (languages, weapons, armor permissions, etc.)
 -- are extended to Dark Iron without duplicating rows.
 UPDATE `playercreateinfo_skills` SET `raceMask` = `raceMask` | @ZandalariTrollMask
-WHERE (`raceMask` & @DwarfMask) <> 0;
+WHERE (`raceMask` & @TrollMask) <> 0;
 
 -- Dedicated Dark Iron racial skill line.
 DELETE FROM `playercreateinfo_skills` WHERE `raceMask` = @ZandalariTrollMask AND `classMask` = 0 AND `skill` = @ZandalariTrollRacials;
