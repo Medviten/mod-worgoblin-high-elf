a5a276646f89f0113514f4a3459dd8a6761c2ab6
Medviten <medviten@protonmail.com>
2026-09-08 23:14:10 +0200

    Trying to fix Zandalari scale and reputation.

--- modpaks/F-035_mod-zandalari-troll/dbc/[BASE,F-035]_creaturedisplayinfo.sql
+++ modpaks/F-035_mod-zandalari-troll/dbc/[BASE,F-035]_creaturedisplayinfo.sql
@@ -4,8 +4,8 @@
 DELETE FROM `creaturedisplayinfo` WHERE `id` IN (@ZandalariTrollMaleDisplay, @ZandalariTrollFemaleDisplay);
 DELETE FROM `creaturedisplayinfo` WHERE `id` BETWEEN 94160 AND 94210; -- Zandalari Troll (same as F-040)
 INSERT INTO `creaturedisplayinfo` (`id`, `model_id`, `sound_id`, `extended_display_info_id`, `creature_model_scale`, `creature_model_alpha`, `texture_variation_1`, `texture_variation_2`, `texture_variation_3`, `portrait_texture_name`, `blood_level`, `blood_id`, `npc_sound_id`, `praticle_color_id`, `creature_geoset_data`, `obj_effect_package_id`) VALUES
-(@ZandalariTrollMaleDisplay, @ZandalariTrollMaleModel, 0, @ZandalariTrollMaleDisplayExtra, '0.75', 255, '', '', '', '', 3, 0, 0, 0, 0, 0),
-(@ZandalariTrollFemaleDisplay, @ZandalariTrollFemaleModel, 0, @ZandalariTrollFemaleDisplayExtra, '0.75', 255, '', '', '', '', 3, 0, 0, 0, 0, 0),
+(@ZandalariTrollMaleDisplay, @ZandalariTrollMaleModel, 0, @ZandalariTrollMaleDisplayExtra, '1', 255, '', '', '', '', 3, 0, 0, 0, 0, 0),
+(@ZandalariTrollFemaleDisplay, @ZandalariTrollFemaleModel, 0, @ZandalariTrollFemaleDisplayExtra, '1', 255, '', '', '', '', 3, 0, 0, 0, 0, 0),
 (94161, 4873, 0, 0, 1, 255, 'druidbearzandalaritroll_blue', '', '', '', 1, 0, 0, 0, 0, 0),
 (94162, 4873, 0, 0, 1, 255, 'druidbearzandalaritroll_dark', '', '', '', 1, 0, 0, 0, 0, 0),
 (94163, 4873, 0, 0, 1, 255, 'druidbearzandalaritroll_green', '', '', '', 1, 0, 0, 0, 0, 0),
--- modpaks/F-035_mod-zandalari-troll/dbc/[BASE,F-035]_faction.sql
+++ modpaks/F-035_mod-zandalari-troll/dbc/[BASE,F-035]_faction.sql
@@ -4,4 +4,4 @@ INSERT INTO `faction` (`id`, `reputation_index`, `reputation_race_mask_1`, `repu
 (@ZandalariTrollFaction, @ZandalariTrollFactionRepIndex, @BarrensBros & ~@ZandalariTrollMask, @AllianceMask, @ZandalariTrollMask, @UndercityMask, 0, 0, 0, 0, 3100, -42000, 4000, 500, 273, 6, 17, 17, 67, '1.0000000000000000', '0.2500000000000000', 7, 5, 'Zuldazar', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', 16712190, 'The capital of the Zandalari Empire', '', '', '', '', '', '', '.', '', '', '', '', '', '', '', '', 16712190);
 
 UPDATE `faction` SET `reputation_race_mask_1` = `reputation_race_mask_1` | @ZandalariTrollMask, `reputation_race_mask_2` = @AllianceMask, `reputation_race_mask_4` = @UndercityMask WHERE `id` IN (@OrcFaction, @TaurenFaction, @TrollFaction);
-UPDATE `faction` SET `reputation_race_mask_1` = @UndercityMask, `reputation_race_mask_2` = @AllianceMask, `reputation_race_mask_4` = @BarrensBros WHERE `id` IN (@OrcFaction, @TaurenFaction, @TrollFaction);
+UPDATE `faction` SET `reputation_race_mask_1` = @UndercityMask, `reputation_race_mask_2` = @AllianceMask, `reputation_race_mask_4` = @BarrensBros WHERE `id` IN (@UndeadFaction, @BloodElfFaction);
