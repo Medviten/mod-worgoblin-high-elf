8abc1dd60f79148fade22ec3de1e5dfe15bfb068
Medviten <medviten@protonmail.com>
2026-09-11 03:28:09 +0200

    Testing 0 rep start

--- modpaks/F-077_mod-faction-free/dbc/[F-077]_faction.sql
+++ modpaks/F-077_mod-faction-free/dbc/[F-077]_faction.sql
@@ -3,6 +3,7 @@
 -- Changed values
 -- swap PEACE_FORCED for AT_WAR on the capital-city reputation slot
 -- 273 - 16 (PEACE_FORCED) + 2 (AT_WAR) = 259
+/*
 UPDATE `faction` SET `reputation_base_2` = 400, `reputation_flags_2` = 273 WHERE `id` = @GoblinFaction; -- Bilgewater Cartel
 UPDATE `faction` SET `reputation_base_2` = 400, `reputation_flags_2` = 273 WHERE `id` = @WorgenFaction; -- Gilneas
 UPDATE `faction` SET `reputation_base_2` = 400, `reputation_flags_2` = 273 WHERE `id` = @HighElfFaction; -- High Elven Loyalists
@@ -10,20 +11,19 @@ UPDATE `faction` SET `reputation_base_2` = 400, `reputation_flags_2` = 273 WHERE
 UPDATE `faction` SET `reputation_base_2` = 400, `reputation_flags_2` = 273 WHERE `id` = @OgreFaction; -- Stonemaul Clan
 UPDATE `faction` SET `reputation_base_2` = 400, `reputation_flags_2` = 273 WHERE `id` = @DarkIronDwarfFaction; -- Shadowforge City
 UPDATE `faction` SET `reputation_base_2` = 400, `reputation_flags_2` = 273 WHERE `id` = @ZandalariTrollFaction; -- Zuldazar
-
+*/
 -- UPDATE `faction` SET `reputation_flags_2` = 259 WHERE `id` IN (@HumanFaction, @OrcFaction, @DwarfFaction, @NightElfFaction, @UndeadFaction, @TaurenFaction, @GnomeFaction, @TrollFaction, @BloodElfFaction, @DraeneiFaction, @GoblinFaction, @WorgenFaction, @HighElfFaction, @MagharOrcFaction, @OgreFaction);
 -- flip PEACE_FORCED (0x10) off, AT_WAR (0x02) on, for the native-race slot
 -- UPDATE `faction` SET `reputation_flags_1` = (`reputation_flags_1` & ~0x10) | 0x02 WHERE `id` IN (@HumanFaction, @OrcFaction, @DwarfFaction, @NightElfFaction, @UndeadFaction, @TaurenFaction, @GnomeFaction, @TrollFaction, @BloodElfFaction, @DraeneiFaction, @GoblinFaction, @WorgenFaction, @HighElfFaction, @MagharOrcFaction, @OgreFaction);
 -- UPDATE `faction` SET `reputation_flags_4` = (`reputation_flags_4` & ~0x10) | 0x02 WHERE `id` IN (@HumanFaction, @OrcFaction, @DwarfFaction, @NightElfFaction, @UndeadFaction, @TaurenFaction, @GnomeFaction, @TrollFaction, @BloodElfFaction, @DraeneiFaction, @GoblinFaction, @WorgenFaction, @HighElfFaction, @MagharOrcFaction, @OgreFaction);
 
-UPDATE `faction` SET `reputation_flags_1` = (`reputation_flags_1` & ~0x10), `reputation_flags_2` = (`reputation_flags_2` & ~0x10), `reputation_flags_3` = (`reputation_flags_3` & ~0x10), `reputation_flags_4` = (`reputation_flags_4` & ~0x10) WHERE `id` IN (
-    @HumanFaction, @OrcFaction, @DwarfFaction, @NightElfFaction, @UndeadFaction, @TaurenFaction, @GnomeFaction, @TrollFaction,
-    @BloodElfFaction, @DraeneiFaction, @GoblinFaction, @WorgenFaction,
-    @HighElfFaction, @MagharOrcFaction, @OgreFaction, @DarkIronDwarfFaction, @ZandalariTrollFaction
-);/*
-UPDATE `faction` SET `reputation_base_2` = 1 WHERE `id` IN (
+UPDATE `faction` SET `reputation_flags_1` = (`reputation_flags_1` & ~0x10), `reputation_flags_2` = (`reputation_flags_2` & ~0x10 | 2), `reputation_flags_3` = (`reputation_flags_3` & ~0x10), `reputation_flags_4` = (`reputation_flags_4` & ~0x10) WHERE `id` IN (
     @HumanFaction, @OrcFaction, @DwarfFaction, @NightElfFaction, @UndeadFaction, @TaurenFaction, @GnomeFaction, @TrollFaction,
     @BloodElfFaction, @DraeneiFaction, @GoblinFaction, @WorgenFaction,
     @HighElfFaction, @MagharOrcFaction, @OgreFaction, @DarkIronDwarfFaction, @ZandalariTrollFaction
 );
-*/
+UPDATE `faction` SET `reputation_base_2` = 0 WHERE `id` IN (
+    @HumanFaction, @OrcFaction, @DwarfFaction, @NightElfFaction, @UndeadFaction, @TaurenFaction, @GnomeFaction, @TrollFaction,
+    @BloodElfFaction, @DraeneiFaction, @GoblinFaction, @WorgenFaction,
+    @HighElfFaction, @MagharOrcFaction, @OgreFaction, @DarkIronDwarfFaction, @ZandalariTrollFaction
+);
