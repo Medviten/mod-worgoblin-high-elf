032df39ad47003bdeb42546c5ab3721e03850e96
Medviten <medviten@protonmail.com>
2026-09-12 00:16:46 +0200

    Put the INSERT statement back in.

--- modpaks/F-030_mod-worgoblin/dbc/[F-030]_creaturedisplayinfo.sql
+++ modpaks/F-030_mod-worgoblin/dbc/[F-030]_creaturedisplayinfo.sql
@@ -2,6 +2,8 @@
 DELETE FROM `creaturedisplayinfo` WHERE `id` BETWEEN 92600 AND 92605;
 DELETE FROM `creaturedisplayinfo` WHERE `id` IN (@WorgenWildMaleDisplay, @WorgenWildFemaleDisplay, @GilneanMaleDisplay, @GilneanFemaleDisplay);
 DELETE FROM `creaturedisplayinfo` WHERE `id` BETWEEN 33650 AND 33669;
+
+INSERT INTO `creaturedisplayinfo` (`id`, `model_id`, `sound_id`, `extended_display_info_id`, `creature_model_scale`, `creature_model_alpha`, `texture_variation_1`, `texture_variation_2`, `texture_variation_3`, `portrait_texture_name`, `blood_level`, `blood_id`, `npc_sound_id`, `praticle_color_id`, `creature_geoset_data`, `obj_effect_package_id`) VALUES
 (92600, 3141, 0, 62100, '1.0000000000000000', 255, '', '', '', '', 0, 0, 0, 0, 0, 0),
 (92601, 3141, 0, 62101, '1.0000000000000000', 255, '', '', '', '', 0, 0, 0, 0, 0, 0),
 (92602, 3142, 0, 62102, '1.0000000000000000', 255, '', '', '', '', 0, 0, 0, 0, 0, 0),
