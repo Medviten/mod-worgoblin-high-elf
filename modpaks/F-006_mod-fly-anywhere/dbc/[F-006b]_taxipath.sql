ce4f338091174d3843d6299f2f616e7c12c1ec78
Medviten <medviten@protonmail.com>
2026-09-12 23:02:21 +0200

    Added SET to make statements

--- modpaks/F-006_mod-fly-anywhere/dbc/[F-006b]_taxipath.sql
+++ modpaks/F-006_mod-fly-anywhere/dbc/[F-006b]_taxipath.sql
@@ -1,6 +1,6 @@
 SET @TaxiPathID = (SELECT COALESCE(MAX(id), 0) FROM dbc.taxipathnode);
-@TaxiAuberdineExodar := @TaxiPathID := @TaxiPathID +1;
-@TaxiExodarAuberdine := @TaxiPathID := @TaxiPathID +1;
+SET @TaxiAuberdineExodar := @TaxiPathID := @TaxiPathID +1;
+SET @TaxiExodarAuberdine := @TaxiPathID := @TaxiPathID +1;
 
 DELETE FROM `taxi_path` WHERE `id` IN (@TaxiAuberdineExodar, @TaxiExodarAuberdine);
 INSERT INTO `taxi_path` (`id`, `fromtaxinode`, `totaxinode`, `cost`) VALUES
