56d5e95a1de55d0931fa6add537224e21e17b41e
Medviten <medviten@protonmail.com>
2026-09-12 23:06:04 +0200

    Fixed order and column names

--- modpaks/F-006_mod-fly-anywhere/dbc/[F-006a]_taxipath.sql
+++ modpaks/F-006_mod-fly-anywhere/dbc/[F-006a]_taxipath.sql
@@ -3,6 +3,6 @@ SET @TaxiAuberdineExodar := @TaxiPathID := @TaxiPathID +1;
 SET @TaxiExodarAuberdine := @TaxiPathID := @TaxiPathID +1;
 
 DELETE FROM `taxipath` WHERE `id` IN (@TaxiAuberdineExodar, @TaxiExodarAuberdine);
-INSERT INTO `taxipath` (`id`, `fromtaxinode`, `totaxinode`, `cost`) VALUES
+INSERT INTO `taxipath` (`id`, `from_taxi_node`, `to_taxi_node`, `cost`) VALUES
 (@TaxiAuberdineExodar, @TaxiNodeAuberdine, @TaxiNodeExodar, 250), -- Auberdine – Exodar
 (@TaxiExodarAuberdine, @TaxiNodeExodar, @TaxiNodeAuberdine, 250); -- Exodar – Auberdine
