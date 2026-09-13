c7b0168b131cc520a92fc98bb188d33847e751e6
Medviten <medviten@protonmail.com>
2026-09-13 09:08:43 +0200

    Changed to IN ()

--- modpaks/F-004_mod-worgoblin/dbc/[BASE,F-004]_spellicon.sql
+++ modpaks/F-004_mod-worgoblin/dbc/[BASE,F-004]_spellicon.sql
@@ -1,7 +1,7 @@
 -- spellicon: 10 inserts, 0 updates, 0 deletes
 
 -- New entries
-DELETE FROM `spellicon` WHERE `id` = (4331, 133032, 366937, 369214, 370211, 370670, 370769, 463874, 463876, 514641);
+DELETE FROM `spellicon` WHERE `id` IN (4331, 133032, 366937, 369214, 370211, 370670, 370769, 463874, 463876, 514641);
 INSERT INTO `spellicon` (`id`, `name`) VALUES
 (4331, 'Interface\\Icons\\ability_racial_bestdealsanywhere'),
 (133032, 'Interface\\Icons\\inv_gizmo_rocketlauncher'),
