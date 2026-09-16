cb50465d311ba07ff9c07e76c2baa6705b5d7a35
Medviten <medviten@protonmail.com>
2026-09-16 21:13:06 +0200

    Fixed race reference

--- modpaks/F-020_mod-pandaren-alliance/dbc/[BASE,F-020]_creaturedisplayinfoextra.sql
+++ modpaks/F-020_mod-pandaren-alliance/dbc/[BASE,F-020]_creaturedisplayinfoextra.sql
@@ -3,5 +3,5 @@
 -- New entries
 DELETE FROM `creaturedisplayinfoextra` WHERE `id` IN (@PandarenMaleDisplayExtra, @PandarenFemaleDisplayExtra);
 INSERT INTO `creaturedisplayinfoextra` (`id`, `race`, `gender`, `skin_color`, `face_type`, `hair_style`, `hair_color`, `facial_hair`, `helm_id`, `shoulders_id`, `shirt_id`, `chest_id`, `belt_id`, `legs_id`, `boots_id`, `wrists_id`, `gloves_id`, `tabard_id`, `cape_id`, `can_equip`, `texture`) VALUES
-(@PandarenMaleDisplayExtra,   @Pandaren, @Male,   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, ''),
-(@PandarenFemaleDisplayExtra, @Pandaren, @Female, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, '');
+(@PandarenMaleDisplayExtra,   @AlliancePandaren, @Male,   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, ''),
+(@PandarenFemaleDisplayExtra, @AlliancePandaren, @Female, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, '');
