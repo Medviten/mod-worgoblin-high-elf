e5a026f25c0db6206f135d6c87989380c2e07b70
Medviten <medviten@protonmail.com>
2026-09-17 00:44:10 +0200

    Fixed ,

--- modpaks/F-021_mod-pandaren-horde/dbc/[F-021]_factiontemplate.sql
+++ modpaks/F-021_mod-pandaren-horde/dbc/[F-021]_factiontemplate.sql
@@ -1,3 +1,3 @@
 DELETE FROM `factiontemplate` WHERE `id` IN (@HordePandarenFactionTemplate);
 INSERT INTO `factiontemplate`(`id`, `faction`, `flags`, `faction_group`, `friend_group`, `enemy_group`, `enemies_1`, `enemies_2`, `enemies_3`, `enemies_4`, `friends_1`, `friends_2`, `friends_3`, `friends_4`) VALUES
-(@HordePandarenFactionTemplate, @HordePandarenPlayer, 72, 5, 4, 10, 0, 0, 0, 0, 0, 0, 0, 0), -- "PLAYER"," HordePandaren", copied from Troll
+(@HordePandarenFactionTemplate, @HordePandarenPlayer, 72, 5, 4, 10, 0, 0, 0, 0, 0, 0, 0, 0); -- "PLAYER"," HordePandaren", copied from Troll
